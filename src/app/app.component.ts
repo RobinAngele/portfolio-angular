c8155cdb0f492065551f4239610bfe7369857245
Robin Angelé <frontend@robin4consulting.com>
2025-07-22 15:50:12 +0200

    Enhances platform-specific AOS initialization
    Injects PLATFORM_ID to conditionally initialize AOS only in the browser environment, improving performance and avoiding unnecessary executions on server-side rendering.
    
    Removes the contact button visibility logic from ngOnInit, streamlining the component's initialization process.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { TranslateService } from "@ngx-translate/core";
+import { isPlatformBrowser } from '@angular/common';
 import AOS from 'aos';
 import { LogoComponent } from './logo/logo.component';
-import { ButtonComponent } from './button/button.component';
 import { BubbleComponent } from './bubble/bubble.component';
-// Import the translations
 import translationsEN from "../../public/i18n/en.json";
 import translationsDE from "../../public/i18n/de.json";
 
@@ -15,7 +14,6 @@ import translationsDE from "../../public/i18n/de.json";
   imports: [
     RouterOutlet, 
     LogoComponent, 
-    ButtonComponent, 
     BubbleComponent
   ],
   templateUrl: './app.component.html',
@@ -26,7 +24,8 @@ export class AppComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private translate: TranslateService
+    private translate: TranslateService,
+    @Inject(PLATFORM_ID) private platformId: Object
   ) {
     translate.setTranslation('en', translationsEN);
     translate.setTranslation('de', translationsDE);
@@ -36,12 +35,8 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    AOS.init();
-  }
-
-  shouldShowContactButton(): boolean {
-    const currentPath = this.router.url;
-    // Hide button on legal notice and privacy policy pages
-    return !currentPath.includes('legal-notice') && !currentPath.includes('privacy-policy');
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init();
+    }
   }
 }
